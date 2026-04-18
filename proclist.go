9497d9d41b1c118cf6084a054944a9bd4946dd3c
Vitali Baumtrok <vbsw@mailbox.org>
2026-04-19 00:16:08 +0200

    list command

--- proclist.go
+++ proclist.go
@@ -12,5 +12,31 @@ import (
 )
 
 func processList(command *tCommand) {
-	fmt.Println("list not implemented, yet")
+	var proc tProcess
+	proc.initInputOutputDir(command)
+	proc.fetchInputSubPaths(command)
+	if command.err == nil {
+		if len(proc.subPaths) > 0 {
+			proc.initOthers(command.threads, command.contentFilter)
+			if command.or {
+				for i := 0; i < proc.threads; i++ {
+					from, to := proc.step(i)
+					go proc.checkContainsAny(command.inputDir, from, to)
+				}
+			} else {
+				for i := 0; i < proc.threads; i++ {
+					from, to := proc.step(i)
+					go proc.checkContainsAll(command.inputDir, from, to)
+				}
+			}
+			for i := 0; i < len(proc.subPaths); i++ {
+				proc.fetchResultsFromChannel(i)
+				if proc.resultsIdx[i] == 1 {
+					fmt.Println(proc.subPaths[i])
+				} else if proc.resultsErr[i] != nil && !command.silent {
+					fmt.Println("Warning:", proc.resultsErr[i].Error())
+				}
+			}
+		}
+	}
 }
